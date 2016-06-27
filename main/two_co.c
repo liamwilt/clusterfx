563b7b327d2ca15ab0ffe46c9dff8a707552c857
liamwilt <liam1wilt@gmail.com>
2016-06-27 00:05:37 -0400

    Efficiency edits; including headers.

--- main/two_co.c
+++ main/two_co.c
@@ -8,100 +8,10 @@
 #include <AL/alc.h>
 #include <AL/alut.h>
 
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 768
-#define M_PI 3.141592653
-#define GLFW_HAND_CURSOR   0x00036004
-#define SIDE_NUM    36      // numnber of triangles we use to draw a circle  
 #define RADIUS      100      // radius of a circle 
-#define DISTANCE    5       // max length of a movement 
-#define TOTAL_TIME  5       // 5 minutes 
-#define CIRCLE_NUM  2       // number of circle
+#define CIRCLE_NUM  2        // number of circles
 
-
-struct Circle {
-    GLfloat x;              // center points
-    GLfloat y;
-    GLfloat z;
-    GLfloat radius;         // circle radius
-    GLint numberOfSides;    // number of side
-    GLfloat xVector;        // horizontal vecotr
-    GLfloat yVector;        // vertical vector
-    GLfloat colour[3];      // color of the circle
-};
-
-typedef struct Circle Circle;
-
-int chosenCircleIndex = 0;
-Circle * currentCircle = NULL;
-GLFWwindow *window;
-GLFWcursor *cursor;
-FILE * globalOut;
-
-void drawCircle(Circle circle);
-
-static void key_callback(GLFWwindow *w, int key, int scancode, int action, int mods);
-
-Circle initCircle(Circle circle);
-/**
- * initialize a circle 
- * @param circle
- * @return an initialized circle 
- */
-
-void initCircleArray(Circle circle[], int length);
-/**
- * initialize array of circles 
- * @param circle array
- * @param length length of the array
- */
-
-int checkCollision(Circle c1, Circle c2);
-/**
- * @param c1 circle 
- * @param c2 circle
- * @return 0 if they don't collide with each other 
- *          1 if they collide
- */
-
-void moves(Circle circle[], int length, int index);
-/**
- * move each circle in the array 
- * @param circle array
- * @param length length of array
- * @param index index of current considered circle
- */
-
-static void error(int error, const char *desc);
-
-void writeData(FILE * out, GLfloat oX, GLfloat oY, GLfloat radius,
-        GLfloat cX, GLfloat cY, double timer,
-        GLfloat distance);
-/* 
- * out      --> output stream
- * oX       --> center of the circle X
- * oY       --> center of the circle Y
- * radius   --> radius of the circle 
- * cX       --> cursor position   
- * cY       --> cursor position      
- * timer    --> current time
- * distance --> distance from cursor to center of the circle 
- */
-
-int isCursorInTheChosenCircle();
-/**
- * 
- * @return 1 the cursor is in the chosen circle
- *          0 the cursor is out of the chosen circle 
- */
-
-
-void writeToFile(FILE * out, Circle circle, double timer, GLFWwindow *window);
-
-void mouse_button_callback(GLFWwindow* window,
-        int button, int action, int mods);
-
-void playSound();
+#include "co.h"
 
 int main(void) {
 
@@ -356,7 +266,7 @@ void moves(Circle circle[], int length, int index) {
     // the current circle will have to avoid all previous considered circles 
     for (i = 0; i < index; i++) {
         // they collide
-        if (checkCollision(c, circle[i]) == 1) {
+        if (testCollision(c, circle[i]) == 1) {
             // 2 circles will travel in opposite directions, post-collision 
             c.xVector = (c.x - circle[i].x) / RADIUS * (rand() % DISTANCE);
             c.yVector = (c.y - circle[i].y) / RADIUS * (rand() % DISTANCE);
@@ -392,7 +302,7 @@ void moves(Circle circle[], int length, int index) {
     circle[index] = c;
 }
 
-int checkCollision(Circle c1, Circle c2) {
+int testCollision(Circle c1, Circle c2) {
     GLfloat distance = sqrt((c1.x - c2.x) * (c1.x - c2.x)
             + (c1.y - c2.y) * (c1.y - c2.y));
     if (distance <= c1.radius * 2.0) {
